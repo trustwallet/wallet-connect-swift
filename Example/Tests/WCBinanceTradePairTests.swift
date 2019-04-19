54984ddaae3c80b597b33d694dcab518aa642fcc
hewigovens <360470+hewigovens@users.noreply.github.com>
2019-04-19 14:17:09 +0800

    Add more tests

--- Example/Tests/WCBinanceTradePairTests.swift
+++ Example/Tests/WCBinanceTradePairTests.swift
@@ -13,17 +13,21 @@ class WCBinanceTradePairTests: XCTestCase {
 
     func testParsing() {
         let symbol = "BNB_ETH.B-261"
-        let pair = WCBinanceTradePair.from(symbol)!
+        let pair = WCBinanceTradePair.from(symbol)
 
-        XCTAssertEqual(pair.from, "BNB")
-        XCTAssertEqual(pair.to, "ETH.B")
-    }
+        XCTAssertEqual(pair?.from, "BNB")
+        XCTAssertEqual(pair?.to, "ETH.B")
 
-    func testParse() {
-        let symbol = "000-0E1_BNB"
-        let pair = WCBinanceTradePair.from(symbol)!
+        let symbol2 = "000-0E1_BNB"
+        let pair2 = WCBinanceTradePair.from(symbol2)
 
-        XCTAssertEqual(pair.from, "000")
-        XCTAssertEqual(pair.to, "BNB")
+        XCTAssertEqual(pair2?.from, "000")
+        XCTAssertEqual(pair2?.to, "BNB")
+
+        let symbol3 = "CRYPRICE-150_BTC.B-918"
+        let pair3 = WCBinanceTradePair.from(symbol3)
+
+        XCTAssertEqual(pair3?.from, "CRYPRICE")
+        XCTAssertEqual(pair3?.to, "BTC.B")
     }
 }
